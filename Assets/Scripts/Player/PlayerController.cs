47e421c79916f98ff6ab70723874ea5c77e699fe
maomaoovo <maomaoovo@outlook.com>
2025-01-18 20:05:41 +0800

    feat : Player朝向 添加了Player碰撞体

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,18 +9,40 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        PlayerMove();
-    }
-
-    void PlayerMove()
-    {
         //Input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 dir = new Vector2(horizontal,vertical);
 
+        PlayerMove(dir);
+        PlayerRotate(dir);
+        PlayerAlwaysTowardMouse();
+    }
+
+    void PlayerMove(Vector2 dir)
+    {
         Vector2 targetPos = new Vector2(transform.position.x+moveSpeed*dir.normalized.x,transform.position.y+moveSpeed*dir.normalized.y);
 
         transform.position = Vector2.Lerp(transform.position,targetPos,Time.deltaTime);
+    }
+
+    void PlayerRotate(Vector2 dir)
+    {
+        if(dir.x != 0 || dir.y != 0)
+        {
+            //向量与y轴正方向的夹角
+            float angle = Mathf.Atan2(dir.x,dir.y)* Mathf.Rad2Deg;
+            //绕z轴旋转angle度
+            transform.rotation = Quaternion.AngleAxis(-angle,Vector3.forward);
+        }
+    }
+
+    void PlayerAlwaysTowardMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //朝向鼠标位置的方向
+        Vector2 towardDir = mousePos - transform.position;
+        float angle = Mathf.Atan2(towardDir.x,towardDir.y)*Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle,Vector3.forward);
     }
 }
