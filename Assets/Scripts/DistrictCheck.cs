508d5d1fe56ebbe9c8ad9bd12538718bce65cc3d
maomaoovo <maomaoovo@outlook.com>
2025-01-19 16:08:23 +0800

    feat : 场景优化

--- Assets/Scripts/DistrictCheck.cs
+++ Assets/Scripts/DistrictCheck.cs
@@ -4,5 +4,42 @@ using UnityEngine;
 
 public class DistrictCheck : MonoBehaviour
 {
-    
+    [SerializeField]
+    private BoxCollider2D victoryDistrict;
+    private int victoryCount = 0;
+
+    private void Update() 
+    {
+        if(victoryCount == 3)TriggerGameWin();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        #if UNITY_EDITOR
+        Debug.Log("TriggerEnter OK!");
+        #endif
+
+        if(other.CompareTag("Bubble"))
+        {
+            victoryCount++;
+        }    
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        #if UNITY_EDITOR
+        Debug.Log("TriggerExit OK!");
+        #endif
+
+        if(other.CompareTag("Bubble"))
+        {
+            victoryCount--;
+        }    
+    }
+
+    void TriggerGameWin()
+    {
+        Time.timeScale = 0f;
+    }
 }
